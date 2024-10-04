2e3e428c15c151a70ccb6d48d8d9b0f24bf5d287
Yady <ygfrta@gmail.com>
2024-10-04 12:41:58 -0500

    Cambios 1 prueba

---
+++ src/main/java/org/example/Carritos.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Carritos {
+    public static void main(String[] args) {
+           int edad=18;
+
+           if (edad>=18)
+           {
+            System.out.println("UD PUEDE REALIZAR EL PROCESO DE VOTACIONES ELECTORALES");
+           }
+
+
+
+    }
+}
